002caf8d313a19f27d9ba04394bbf4a3ac531f3c
yi xuan <3a832061@gm.student.ncut.edu.tw>
2021-10-25 22:26:30 +0800

    完成

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 define('DB_PORT', 33060);
 define('DB_DATABASE', 'lara_0804_p');
 define('DB_CHARSET', 'utf8');
-define('DB_USERNAME',' root');
+define('DB_USERNAME','root');
 define('DB_PASSWORD', 'root');
